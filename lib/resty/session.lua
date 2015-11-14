7d0a5e4045ae5b29a9622ab3c7aad70c54cf2a16
LuaCI <luaci@luadist.org>
2015-11-14 01:18:00 +0100

    Update to version 1.3-1

--- lib/resty/session.lua
+++ lib/resty/session.lua
@@ -1,18 +1,19 @@
-local base64enc  = ngx.encode_base64
-local base64dec  = ngx.decode_base64
-local ngx_var    = ngx.var
-local concat     = table.concat
-local hmac       = ngx.hmac_sha1
-local time       = ngx.time
-local type       = type
-local json       = require "cjson"
-local aes        = require "resty.aes"
-local ffi        = require "ffi"
-local ffi_cdef   = ffi.cdef
-local ffi_new    = ffi.new
-local ffi_str    = ffi.string
-local ffi_typeof = ffi.typeof
-local C          = ffi.C
+local base64enc   = ngx.encode_base64
+local base64dec   = ngx.decode_base64
+local ngx_var     = ngx.var
+local concat      = table.concat
+local hmac        = ngx.hmac_sha1
+local time        = ngx.time
+local cookie_time = ngx.cookie_time
+local type        = type
+local json        = require "cjson"
+local aes         = require "resty.aes"
+local ffi         = require "ffi"
+local ffi_cdef    = ffi.cdef
+local ffi_new     = ffi.new
+local ffi_str     = ffi.string
+local ffi_typeof  = ffi.typeof
+local C           = ffi.C
 
 local ENCODE_CHARS = {
     ["+"] = "-",
@@ -72,7 +73,10 @@ function setcookie(session, value, expires)
     local cookie = { session.name, "=", value }
     local domain = session.cookie.domain
     if expires then
-        cookie[#cookie + 1] = "; Expires=Thu, 01 Jan 1970 00:00:01 GMT"
+        cookie[#cookie + 1] = "; Expires=Thu, 01 Jan 1970 00:00:01 GMT; Max-Age=0"
+    elseif session.cookie.persistent then
+        cookie[#cookie + 1] = "; Expires="
+        cookie[#cookie + 1] = cookie_time(session.expires)
     end
     if domain ~= "localhost" then
         cookie[#cookie + 1] = "; Domain="
@@ -125,17 +129,20 @@ local function getcookie(cookie)
     return decode(r[1]), tonumber(r[2]), decode(r[3]), decode(r[4])
 end
 
+local persistent = enabled(ngx_var.session_cookie_persistent or false)
 local session = {
-    _VERSION = "1.1",
+    _VERSION = "1.3",
     name = ngx_var.session_name or "session",
     cookie = {
-        renew    = tonumber(ngx_var.session_cookie_renew)    or 600,
-        lifetime = tonumber(ngx_var.session_cookie_lifetime) or 3600,
-        path     = ngx_var.session_cookie_path               or "/",
-        domain   = ngx_var.session_cookie_domain,
-        secure   = enabled(ngx_var.session_cookie_secure),
-        httponly = enabled(ngx_var.session_cookie_httponly   or true)
+        persistent = persistent,
+        renew      = tonumber(ngx_var.session_cookie_renew)    or 600,
+        lifetime   = tonumber(ngx_var.session_cookie_lifetime) or 3600,
+        path       = ngx_var.session_cookie_path               or "/",
+        domain     = ngx_var.session_cookie_domain,
+        secure     = enabled(ngx_var.session_cookie_secure),
+        httponly   = enabled(ngx_var.session_cookie_httponly   or true)
     }, check = {
+        ssi    = enabled(ngx_var.session_check_ssi    or persistent == false),
         ua     = enabled(ngx_var.session_check_ua     or true),
         scheme = enabled(ngx_var.session_check_scheme or true),
         addr   = enabled(ngx_var.session_check_addr   or false)
@@ -165,7 +172,7 @@ function session.start(opts)
         self.cookie.domain = ngx_var.host
     end
     self.key = concat{
-        ssi                                           or "",
+        self.check.ssi    and ssi                     or "",
         self.check.ua     and ngx_var.http_user_agent or "",
         self.check.addr   and ngx_var.remote_addr     or "",
         self.check.scheme and ngx_var.scheme          or ""
--- lua-resty-session-1.3-1.rockspec
+++ lua-resty-session-1.3-1.rockspec
@@ -1,13 +1,13 @@
 package = "lua-resty-session"
-version = "1.2-1"
+version = "1.3-1"
 -- LuaDist source
 source = {
-  tag = "1.2-1",
+  tag = "1.3-1",
   url = "git://github.com/ld-test/lua-resty-session.git"
 }
 -- source = {
 --     url = "git://github.com/bungle/lua-resty-session.git",
---     branch = "v1.2"
+--     branch = "v1.3"
 -- }
 description = {
     summary = "Session library for OpenResty implementing Secure Cookie Protocol",
